b9c244b32bc2d1447f056bc1e386475b79c01c96
apple <ibrahimirfan.main@gmail.com>
2025-02-24 10:59:55 +0700

    baim commit

--- soal4/matrix.h
+++ soal4/matrix.h
@@ -25,23 +25,36 @@ Matrix newMatrix(int a, int b, int c, int d){
 }; 
 
 void printMatrix(Matrix m){
-
+	printf("[ %d %d ]\n", m.a, m.b);
+	printf("[ %d %d ]\n", m.c, m.d);
 };
 
 Matrix add(Matrix m1, Matrix m2){
-
+	Matrix result;
+	result.a = m1.a + m2.a;
+	result.a = m1.b + m2.b;
+	result.a = m1.c + m2.c;
+	result.a = m1.d + m2.d;
+	return result;
 };
 
 Matrix subtract(Matrix m1, Matrix m2){
-
+	Matrix result;
+	result.a = m1.a - m2.a;
+	result.a = m1.b - m2.b;
+	result.a = m1.c - m2.c;
+	result.a = m1.d - m2.d;
+	return result;
 };
 
 void transpose(Matrix *m){
-
+	int temp = m->b;
+	m->b = m->c;
+	m->c = temp;
 };
 
 int determinant(Matrix m){
-
+	return (m.a * m.d) - (m.b * m.c);
 };
 
 #endif 
